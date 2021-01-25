5c551e57e9f41416ffab4f91b03a5815748af5a1
Ángela Jiang-Wang <75572488+angelajw@users.noreply.github.com>
2021-01-25 16:52:11 +0100

    Update data cleaning for Qualtrics.R

--- data cleaning for Qualtrics.R
+++ data cleaning for Qualtrics.R
@@ -4,14 +4,14 @@
 #R version used: 4.0.3 (2020-10-10) -- "Bunny-Wunnies Freak Out"
 
 #Hi all, many Qualtrics surveys used for experiments produce really similar output datasets
-#I will share with you some of my preferred codes for data cleaning and filtering, and also for creating new variables in the dataframe from existing data with functions and logical operators.
+#I will share with you some of my preferred codes and their respective explanations for data cleaning and filtering, and also for creating new variables in the dataframe from existing data with functions and logical operators.
 #As often happens with coding, there are many different ways of doing one same task in R. These are the codes I like using and that normally work for me.
 #Hopefully this may help you so you don't get too crazy looking in forums and all over the internet like me for doing some simple stuff.
 
 #Before starting, I'd like to tell you that you can make data cleaning much more efficient and easier with the right survey design in Qualtrics.
 #For instance, you can edit how you want your variables to be named in the output dataset and how the values of each variable should be coded.
 #Try playing with features such as Question names, Recode values, Question export tags (for questions with many items such as matrices) in order to produce a more "refined" dataset from Qualtrics.
-#I will provide codes for "unrefined" datasets, that is to say, for cases where the aforementioned features are not exploited. 
+#I will provide codes for an "unrefined" dataset, that is to say, for a case where the aforementioned features are not exploited. 
 #Note that with a "refined" dataset, you will use really similar codes but just skipping some steps
 #If you have any questions or comments, feel free to email me at: angelajw11@gmail.com
 
